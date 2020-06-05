3385e60ba49b65eb1aa4129382e422bf55b8f6e5
Hemant soni <sonihemant4741@gmail.com>
2020-06-06 03:01:07 +0530

    form_for with update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,22 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to edit_user_path(@user)
     else
       render :new
+    end
+  end
+
+  def edit
+   @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
   
