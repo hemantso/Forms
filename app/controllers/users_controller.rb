be94f3a5a7b3a526a24ccaddcf2cd829b2357985
Hemant soni <sonihemant4741@gmail.com>
2020-06-07 17:23:46 +0530

    create method redirect to new user path on save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to edit_user_path(@user)
+      redirect_to new_user_path(@user)
     else
       render :new
     end
